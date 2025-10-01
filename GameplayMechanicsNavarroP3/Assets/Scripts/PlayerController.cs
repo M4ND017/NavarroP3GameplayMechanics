82b95e62ed6bf9f61efeba2e42c5431b1606adc8
M4ND017 <732296@student.dusd.net>
2025-10-01 11:40:36 -0700

    finished 4.3
    finished all of 4.3 and starting 4.4

--- GameplayMechanicsNavarroP3/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsNavarroP3/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public bool hasPowerup;
     public float speed = 5.0f;
+    public GameObject powerupIndicator;
+    private float powerupStrength = 15.0f;
     private GameObject focalPoint;
     private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,5 +21,37 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Verticle");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, 0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.transform.position - transform.position);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to  " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
+                
     }
 }
